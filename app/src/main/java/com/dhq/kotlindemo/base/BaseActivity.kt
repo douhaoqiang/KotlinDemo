98cd9546caa35c4228836edb6e4897bc5ab3a2de
douhaoqiang <douhaoqiang@126.com>
2017-08-16 10:49:36 +0800

    base

--- app/src/main/java/com/dhq/kotlindemo/base/BaseActivity.kt
+++ app/src/main/java/com/dhq/kotlindemo/base/BaseActivity.kt
@@ -2,12 +2,20 @@ package com.dhq.kotlindemo.base
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.LayoutInflater
+import android.widget.FrameLayout
+import com.dhq.kotlindemo.R
 
 abstract class BaseActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(getLayoutId())
+        setContentView(R.layout.activity_base);
+        var content = findViewById(R.id.fl_base_content) as FrameLayout;
+        LayoutInflater.from(this).inflate(getLayoutId(), content)
+
+        initView();
+
     }
 
 
